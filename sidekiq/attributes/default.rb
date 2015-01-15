33a6201a7ae3a7db49ad89bab25ac1500a8eb49e
Donald Piret <donald@donaldpiret.com>
2015-01-15 11:49:26 +0800

    Fix

--- sidekiq/attributes/default.rb
+++ sidekiq/attributes/default.rb
@@ -4,6 +4,6 @@ default[:sidekiq] = {}
 
 node[:deploy].each do |application, deploy|
   default[:sidekiq][application.intern] = {}
-  default[:sidekiq][application.intern][:restart_command] = "sudo monit restart -g sidekiq_#{application}_group"
+  default[:sidekiq][application.intern][:restart_command] = "sleep 60 && sudo monit restart -g sidekiq_#{application}_group"
   default[:sidekiq][application.intern][:syslog] = false
 end
